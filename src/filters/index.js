0e2e34c49082d32c3b2443916cf555b8c7085a9b
luanth <luanth@gmail.com>
2019-11-17 21:28:02 +0700

    thử dùng cách sinh filter trực tiếp

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,7 +1,7 @@
 // Import all vue component to comps
 import * as string from './string'
 import * as array from './array'
-import { isArray, isString, makeFilter } from './util'
+import { isArray, isString } from './util'
 
 const transforms = { ...string, ...array }
 
@@ -21,16 +21,16 @@ const GlobalVue = typeof window !== 'undefined' ? window.Vue
 if (GlobalVue) GlobalVue.use({ install })
 
 // auto generated plugin for each of components
-let filters = {}
-Object.keys(transforms).forEach(name => {
-	const install = (Vue) => {
-		if (install.installed) {return} else install.installed = true
-		Vue.filter(name, transforms[name])
-	}
-	// install.installed = false
-
-	filters[name + 'Filter'] = { install }
-})
+// let filters = {}
+// Object.keys(transforms).forEach(name => {
+// 	const install = (Vue) => {
+// 		if (install.installed) {return} else install.installed = true
+// 		Vue.filter(name, transforms[name])
+// 	}
+// 	// install.installed = false
+//
+// 	filters[name + 'Filter'] = { install }
+// })
 
 // To allow use as module(npm / webpack / etc.) export component
 export default {
@@ -39,17 +39,20 @@ export default {
 
 export * from './string'
 export * from './array'
+export const filter = function (func) {
+	return { install: (Vue) => Vue.filter(func.name, func) }
+}
 
-export const uppercaseFilter = makeFilter(string.capitalize)
-export const CurrencyFilter = makeFilter(string.currency)
-export const LowercaseFilter = makeFilter(string.lowercase)
-export const PlaceholderFilter = makeFilter(string.placeholder)
-export const PluralizeFilter = makeFilter(string.pluralize)
-export const TrimFilter = makeFilter(string.trim)
-export const TruncateFilter = makeFilter(string.truncate)
-
-export const UppercaseFilter = makeFilter(string.uppercase)
-export const FilterByilter = makeFilter(array.filterBy)
-export const FindByFilter = makeFilter(array.findBy)
-export const LimitByFilter = makeFilter(array.limitBy)
-export const OrderByFilter = makeFilter(array.orderBy())
+// export const uppercaseFilter = makeFilter(string.capitalize)
+// export const CurrencyFilter = makeFilter(string.currency)
+// export const LowercaseFilter = makeFilter(string.lowercase)
+// export const PlaceholderFilter = makeFilter(string.placeholder)
+// export const PluralizeFilter = makeFilter(string.pluralize)
+// export const TrimFilter = makeFilter(string.trim)
+// export const TruncateFilter = makeFilter(string.truncate)
+//
+// export const UppercaseFilter = makeFilter(string.uppercase)
+// export const FilterByilter = makeFilter(array.filterBy)
+// export const FindByFilter = makeFilter(array.findBy)
+// export const LimitByFilter = makeFilter(array.limitBy)
+// export const OrderByFilter = makeFilter(array.orderBy())
