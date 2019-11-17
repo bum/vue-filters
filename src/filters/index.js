ac42bce0d86ea09fe2f6e7c594f86a2d03d38b71
luanth <luanth@gmail.com>
2019-11-17 13:35:35 +0700

    compare at for each keys

--- src/filters/index.js
+++ src/filters/index.js
@@ -6,11 +6,7 @@ const filters = { capitalize }
 // Declare install function executed by Vue.use()
 export function install(Vue) {
 	if (install.installed) {return} else {install.installed = true}
-	for (let name in filters) {
-		if (typeof filters[name] !== 'function') {
-			Vue.filter(name, filters[name])
-		}
-	}
+	Object.keys(filters).forEach(name => Vue.filter(name, filters[name]))
 }
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
