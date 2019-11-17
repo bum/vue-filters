ff7f89ac03fe37163c52d596e6e037f654b6febb
luanth <luanth@gmail.com>
2019-11-17 18:02:12 +0700

    install with options.filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,13 +1,18 @@
 // Import all vue component to comps
 import * as string from './string'
 import * as array from './array'
+import { isArray, isString } from './util'
 
 const filters = { ...string, ...array }
 
 // Declare install function executed by Vue.use()
-export function install(Vue) {
+export function install(Vue, options = {}) {
 	if (install.installed) {return} else {install.installed = true}
-	Object.keys(filters).forEach(name => Vue.filter(name, filters[name]))
+	const names = !options.filters ? Object.keys(filters)
+		: isArray(options.filters) ? options.filters
+			: isString(options.filters) ? options.filters.trim().split(' ')
+				: Object.keys(filters)
+	names.forEach(name => filters[name] && Vue.filter(name, filters[name]))
 }
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
