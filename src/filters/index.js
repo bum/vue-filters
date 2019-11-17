c7e31c23170594a56f91f54a73b5327cf76bd5f2
luanth <luanth@gmail.com>
2019-11-17 21:37:44 +0700

    thử xuất lại bỏ "es6": true, "browser": true

--- src/filters/index.js
+++ src/filters/index.js
@@ -21,20 +21,21 @@ const GlobalVue = typeof window !== 'undefined' ? window.Vue
 if (GlobalVue) GlobalVue.use({ install })
 
 // auto generated plugin for each of components
-// let filters = {}
-// Object.keys(transforms).forEach(name => {
-// 	const install = (Vue) => {
-// 		if (install.installed) {return} else install.installed = true
-// 		Vue.filter(name, transforms[name])
-// 	}
-// 	// install.installed = false
-//
-// 	filters[name + 'Filter'] = { install }
-// })
+let filters = {}
+Object.keys(transforms).forEach(name => {
+	const install = (Vue) => {
+		if (install.installed) {return} else install.installed = true
+		Vue.filter(name, transforms[name])
+	}
+	// install.installed = false
+
+	filters[name + 'Filter'] = { install }
+})
 
 // To allow use as module(npm / webpack / etc.) export component
 export default {
 	install,
+	...filters,
 }
 
 export * from './string'
